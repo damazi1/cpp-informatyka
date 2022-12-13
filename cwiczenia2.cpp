b7271fda9b3b36702ef430b778aeca795b12e05d
damazi1 <114773869+damazi1@users.noreply.github.com>
2022-12-13 15:50:07 +0100

    lekcjacpp2
    druga lekcja z c++

---
+++ cwiczenia2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//  const unsigned int z = 5;
+//  cout << z;
+
+// //  *int a; taki zapis jest nieprawidłowy
+//  int *a; // a jest wskaźnikiem na liczbę całkowoitą 
+// int**c; // c jest wskaźnikiem do wskaźnika typu całkowitego
+// int d[5]; // d jest tablicą 5 elementową obiektów typu całkowitego
+// short int f[2][4]; // tablica 2 wymiarowa o wymiarach 2 na cztery
+// int(*e)[]; // wkazuje na tablice liczb całkowitych 
+// const float * a; // oby dwie formy są poprawne  
+// float const * a; 
+// const float const * b=&a;//Nie działa nie poprawna skłądnia 
+// const float * const b=&a;//poprawna składnia
+// float const * const b=&a;//poprawna składnia
+// float const const * b=&a;// Nie działa nie poprawna skłądnia 
+return 0;
+
+}
