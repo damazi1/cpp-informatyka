9ba3c1aa0cfee7c6abdb7ca7cb5fea724cba697a
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-02-27 20:10:36 +0100

    podstawy

---
+++ podstawycpp/zad1.2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void wczytanie(){
+    cout<<"wczytywanie() \n";
+}
+void sortowanie(){
+    cout<<"sortowanie() \n";
+}
+void zliczanie(){
+    cout<<"zliczanie() \n";
+}
+void drukowanie(){
+    cout<<"drukowanie() \n";
+}
+int main(){
+    wczytanie();
+    sortowanie();
+    zliczanie();
+    drukowanie();
+
+    return 0;
+}
---
+++ podstawycpp/zad1.5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string wyraz;
+    while(cin>>wyraz){
+        cout<<"przeczytany wyraz: "<< wyraz <<endl;
+    }
+    cout<<"To wszystko \n";
+    return 0;
+}
