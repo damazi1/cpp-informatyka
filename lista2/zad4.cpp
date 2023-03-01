4bc18b93abf0443c0cd94c1c5e22450f4cf059fe
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-03-01 21:54:45 +0100

    lista2

--- lista2/zad4.cpp
+++ lista2/zad4.cpp
@@ -7,7 +7,7 @@ void arg_wartosc(int a,int b){
     int pom=a;
     a=b;
     b=pom;
-    cout <<"wartosc a: "<<a<<" wartosc b: "<<b;
+    cout<<"wartosc a po zamianie: "<<a<<" wartosc b: "<<b<<endl;
 }
 void arg_referencja(int &a,int &b){
     int c=a;
@@ -21,12 +21,15 @@ void arg_wskaznik(int *a,int *b){
 }
 int main(){
     int a=5,b=10;
+
+    cout<<"wartosc a: "<<a<<" wartosc b: "<<b<<endl;
+    arg_wartosc(a,b);
     cout<<"wartosc a: "<<a<<" wartosc b: "<<b<<endl;
     arg_referencja(a,b);
     cout<<"wartosc a po zamianie: "<<a<<" wartosc b: "<<b<<endl;
     
     cout<<"wartosc a: "<<a<<" wartosc b: "<<b<<endl;
-    arg_wskaznik(*a,*b);
+    arg_wskaznik(&a,&b);
     cout<<"wartosc a po zamianie: "<<a<<" wartosc b: "<<b<<endl;
     
     return 0;
---
+++ lista2/zad5.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+using namespace std;
+
+void wypisz(const int* t,const unsigned int n){
+    for (int i=0;i<n;i++){
+        cout<<i<<" elemnet tablicy to: "<<t[i]<<endl;
+    }
+}
+int* utworz(const unsigned int n){
+    int*t=new int [n];
+    return t;
+}
+
+void utworz(int*& t,const unsigned int n){
+    t=new int [n];
+    // cout<<t<<endl;
+}
+void utworz1(int*t,const unsigned int n){
+    t=new int [n];
+    cout<<t;
+}
+void wypelnij(int *t,const unsigned int n){
+    for (int i=0;i<n;i++){
+        if(i%2==0){
+            t[i]=(rand()%20)-25;
+        }
+        else{
+            t[i]=(rand()%20)+5;
+        }
+    }
+}
+void usuwanie(int*t,unsigned int &n){
+    int x;
+    int pom;
+    cout<<"podaj element który chcesz usunąć: ";
+    cin>>x;
+    for (int i=0;i<n;i++){
+        if(t[i]==x){
+            pom=i;
+            break;
+        }
+    }
+    for (int i=pom;i<n;i++){
+        t[i]=t[i+1];
+    }
+    n--;
+}
+void dodaj(int*t,unsigned int &n){
+    int x;
+    cout<<"podaj element który chcesz dodać: ";
+    cin>>x;
+    n++;
+    t[n-1]=x;
+}
+
+int*suma(const int *t1,const int *t2,const unsigned int n){
+    int *t3=new int[n];
+    for (int i=0;i<n;i++){
+        t3[i]=t1[i]+t2[i];
+    }
+    return t3;
+}
+void suma(const int*t1,const int*t2,int*& t3, const unsigned int n){
+    for (int i=0;i<n;i++){
+        cout<<t1[i]<<"+"<<t2[i];
+        t3[i]=t1[i]+t2[i];
+        cout<<"="<<t3[i]<<endl;
+    }
+
+}
+void suma1(const int *t1,const int*t2,int*t3,const unsigned int n){
+    for(int i=0;i<n;i++){
+        cout<<t1[i]<<"+"<<t2[i];
+        t3[i]=t1[i]+t2[i];
+        cout<<"="<<t3[i]<<endl;
+    }
+}
+void tabliczka_mnozenia(){
+    int tab[99];
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=10;j++){
+            tab[1]
+        }
+    }
+}
+
+int main(){
+    srand(time(NULL));
+    unsigned int n;
+    cin>>n;
+    int *t;
+    // utworz(t,n);
+    // cout<<"\n";
+    // cout << endl<<t<<endl;
+    // wypelnij(t,n);
+    // wypisz(t,n);
+    // usuwanie(t,n);
+    // wypisz(t,n);
+    // dodaj(t,n);
+    // wypisz(t,n);
+
+    int *t1,*t2,*t3;    
+    utworz(t1,n);
+    wypelnij(t1,n);
+    utworz(t2,n);
+    wypelnij(t2,n);
+    utworz(t3,n);
+    // cout<<suma(t1,t2,n)<<endl;
+    // suma(t1,t2,t3,n);
+    // wypisz(t3,n);
+    suma1(t1,t2,t3,n);
+    wypisz(t3,n);
+    if(t){
+        delete[]t;
+        t=0;
+    }
+    if(t1){
+        delete[]t;
+        t=0;
+    }
+    if(t2){
+        delete[]t;
+        t=0;
+    }
+    return 0;
+}
