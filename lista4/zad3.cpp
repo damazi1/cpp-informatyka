3576e1b681fb92724f03d19064ebd0177626e872
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-03-16 12:32:36 +0100

    lista4

--- lista4/zad3.cpp
+++ lista4/zad3.cpp
@@ -10,15 +10,26 @@ using namespace std;
 bool palindrom(const char*src){
     size_t n=strlen(src);
     size_t i=0,j=n-1;
+    // for(;i<n;i++){
+    //     if(src[i]=='?'||'.'||','||'-'||'!'||'('||')'){
+            
+    // }
     for (;i<j;i++,j--){
+        if(src[i]==' '||'?'||'.'||','||'-'||'!'||'('||')'){
+            i++;
+        }
+        if(src[j]==' '||'?'||'.'||','||'-'||'!'||'('||')'){
+            j--;
+        }
         if(src[i]!=src[j]){
             return false;
-        }
     }
+    }
+    cout<<"Twoje zdanie jest palindromem"<<endl;
     return true;
 }
 
 int main(int argc,char*argv[]){
-    cout<<"\n"<<palindrom(argv[1])<<endl;
+    cout<<"\n"<<palindrom("a!?la ala")<<endl;
     return 0;
 }
