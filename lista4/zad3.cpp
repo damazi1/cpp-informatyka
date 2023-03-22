340006fa6e25426a72b4401766c400d940c43754
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-03-22 18:20:31 +0100

    palindrom

--- lista4/zad3.cpp
+++ lista4/zad3.cpp
@@ -7,34 +7,50 @@
 
 using namespace std;
 
-bool palindrom(const char*src){
+void palindrom(char*src){
     size_t n=strlen(src);
-    size_t i=0,j=n-1;
-    for (;i<j;i++,j--){
-        while(src[i]==' ')i++;
-        while(src[j]==' ')j--;
-        // while(src[i]=='?')i++;
-        // while(src[j]=='?')j--;
-        while(src[i]=='!')i++;
-        while(src[j]=='!')j--;
-        // while(src[i]=='.')i++;
-        // while(src[j]=='.')j--;
-        while(src[i]==',')i++;
-        while(src[j]==',')j--;        //usunąć znaki białe od <a i większe niż z>
+    size_t i=0,j=n;
+    char *palin=new char[n];
+    for (;i<j;i++){
+        palin[i]=toupper(src[i]);
+        cout<<palin[i];
+    }
+    i=0;
+    j=n;
+    cout<<endl;
+    int *tab=new int [n];
+    for(;i<j;i++){
+        tab[i]=(int)palin[i];
+    }
+    i=0;
+    for(;i<j;i++){
+        if(tab[i]>=65){
+            palin[i].replace('')
+        }
+        if(tab[i]<=90){
+            src[i]=palin[i];
+        }
+    }
+    cout<<endl;
+    for(i=0;i<j;i++){
+        cout<<src[i];
+    }
+    i=0;
+    j=n-1;
+    for(;i<j;i++,j--){
         if(src[i]!=src[j]){
-            return false;
+            return;
+        }
     }
-    }
-    cout<<"Twoje zdanie jest palindromem"<<endl;
-    return true;
+    cout<<"\nTwoje zdanie jest palindromem"<<endl;
 }
 
 int main(int argc,char*argv[]){
-    if(argc!=2){
-        cerr<<"Nieprawidłowa ilośc przekazanych argumentów(2)!"<<endl;
-        cout<<"Wymagane argumenty to: ./nazwa,zdanie"<<endl;
-        return 1;
-    }
-    cout<<"\n"<<palindrom(argv[1])<<endl;
+    // if(argc!=2){
+    //     cerr<<"Nieprawidłowa ilośc przekazanych argumentów(2)!"<<endl;
+    //     cout<<"Wymagane argumenty to: ./nazwa,zdanie"<<endl;
+    //     return 1;
+    // }
+    palindrom("Ala mam AlA");
     return 0;
 }
