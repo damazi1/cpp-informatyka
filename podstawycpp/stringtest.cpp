f275731510cb5a6e7f8f7b02ecc8fcd576ebc520
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-04-02 13:58:36 +0200

    Update stringtest.cpp

--- podstawycpp/stringtest.cpp
+++ podstawycpp/stringtest.cpp
@@ -1,24 +1,23 @@
-// inserting into a string
+// strings and c-strings
 #include <iostream>
+#include <cstring>
 #include <string>
 
 int main ()
 {
-  std::string str="to be question";
-  std::string str2="the ";
-  std::string str3="or not to be";
-  std::string::iterator it;
+  std::string str ("Please split this sentence into tokens");
 
-  // used in the same order as described above:
-  str.insert(6,str2);                 // to be (the )question
-  str.insert(6,str3,3,4);             // to be (not )the question
-  str.insert(10,"that is cool",8);    // to be not (that is )the question
-  str.insert(10,"to be ");            // to be not (to be )that is the question
-  str.insert(15,1,':');               // to be not to be(:) that is the question
-  it = str.insert(str.begin()+5,','); // to be(,) not to be: that is the question
-  str.insert (str.end(),3,'.');       // to be, not to be: that is the question(...)
-  str.insert (it+2,str3.begin(),str3.begin()+3); // (or )
+  char * cstr = new char [str.length()+1];
+  std::strcpy (cstr, str.c_str());
 
-  std::cout << str << '\n';
+  // cstr now contains a c-string copy of str
+
+  char * p = std::strtok (cstr," ");
+  while (p!=0)
+  {
+    std::cout << p << '\n';
+    p = std::strtok(NULL," ");
+  }
+
+  delete[] cstr;
   return 0;
-}
