8e2ac1726e9fea7d0dc42a5415a9441aa6d35c24
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-04-25 20:50:32 +0200

    Create zad1.cpp

---
+++ lista10/zad1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct SPunkt {
+    int x,y;
+};
+
+struct SProsta {
+    int x,y;
+};
+
+struct SOkrag{
+    int x,y;
+};
+
+struct SRownanie{
+    int x,y,z;
+};
+
+int main(){
+
+    return 0;
+}
