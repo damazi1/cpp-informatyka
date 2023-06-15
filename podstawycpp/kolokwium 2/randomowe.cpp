d3c7ab48bffbf07f2cf9ef51e331bd07d4e6f703
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-06-15 20:37:20 +0200

    rand

--- podstawycpp/kolokwium 2/randomowe.cpp
+++ podstawycpp/kolokwium 2/randomowe.cpp
@@ -8,13 +8,13 @@ float losuj(int min = 0, int max = 10)
 {
     float los = 0;
     los = (float)rand()/(float)RAND_MAX*(max-min)+min;
+    los = (float)rand()%max/(float)RAND_MAX;
     return los;
 }
 
 int main(int argc,char**argv)
 {
     srand(time(NULL));
-    int a=atoi(argv[1]);
-    cout<<a<<endl;
+    cout<<losuj(0,15);
     return 0;
 }
