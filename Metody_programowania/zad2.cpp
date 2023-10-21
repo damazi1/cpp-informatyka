7efe8b69458e9a4a993ea63a0c83dc3c52f18ab5
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-10-21 13:17:21 +0200

    zad3

--- Metody_programowania/zad2.cpp
+++ Metody_programowania/zad2.cpp
@@ -1,123 +1,140 @@
 #include <iostream>
 #ifdef _WIN32
-  #include <windows.h>
-#endif 
+#include <windows.h>
+#endif
 using namespace std;
 
-class adres{
-	string miasto,ulica;
+class adres
+{
+	string miasto, ulica;
 	int nr;
-	
+
 public:
 	adres()
 	{
-		miasto="brak";
-		ulica="brak";
-		nr=0;
+		miasto = "brak";
+		ulica = "brak";
+		nr = 0;
 	}
-	adres(const string& a1, const string& a2, int a3):miasto(a1),ulica(a2),nr(a3){}
-	
-	ostream& view(ostream& out)const{
-    return out << miasto << ", " << ulica << ", " << nr << '\n';
-  }
-	/*
-	string& miasto(){
-		return miasto;
-		}
-		*/
+	adres(const string &a1, const string &a2, int a3) : miasto(a1), ulica(a2), nr(a3) {}
 
-  friend 
-  ostream& operator<< (ostream& out, const adres& r);
+	ostream &view(ostream &out) const
+	{
+		return out << miasto << ", " << ulica << ", " << nr << '\n';
+	}
+
+	string &miasto1()
+	{
+		return miasto;
+	}
+
+	friend ostream &operator<<(ostream &out, const adres &r);
 };
-ostream& operator<< (ostream& out, const adres& r){
-  return r.view(out);  
+ostream &operator<<(ostream &out, const adres &r)
+{
+	return r.view(out);
 }
 
-class osoba{
+class osoba
+{
 	string im;
 	int wi;
-	adres* adr;
-	
+	adres *adr;
+
 public:
-	osoba(){
-		im="";
-		wi=0;
-		adr=new adres;
-		}
-	osoba(const string& a1, int a2, const adres& a3):im(a1), wi(a2), adr(new adres(a3)){}
-	
-	osoba(const osoba& a1):im(a1.im),wi(a1.wi),adr(new adres(*a1.adr)){}
-	
-	osoba& operator=(const osoba& a1){
-		if(this!=&a1){
-			im=a1.im;
-			wi=a1.wi;
-			*adr=*a1.adr;
+	osoba()
+	{
+		im = "";
+		wi = 0;
+		adr = new adres;
+	}
+	osoba(const string &a1, int a2, const adres &a3) : im(a1), wi(a2), adr(new adres(a3)) {}
+
+	osoba(const osoba &a1) : im(a1.im), wi(a1.wi), adr(new adres(*a1.adr)) {}
+
+	osoba &operator=(const osoba &a1)
+	{
+		if (this != &a1)
+		{
+			im = a1.im;
+			wi = a1.wi;
+			*adr = *a1.adr;
 		}
 		return *this;
 	}
 
-	~osoba(){
-		if(adr){
+	~osoba()
+	{
+		if (adr)
+		{
 			delete adr;
-			adr=nullptr;
+			adr = nullptr;
 		}
 	}
-	/*
-	string& miasto(){return adr.miasto();}
-	*/
-	
-	ostream& view(ostream& out)const{
-    return out << im << ", " << wi << ", " << *adr << '\n';
-  }
 
-  friend 
-  ostream& operator<< (ostream& out, const osoba& r);
-};
-ostream& operator<< (ostream& out, const osoba& r){
-  return r.view(out);  
-}
-int main(){
-	#ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8); 
-	#endif
-	adres* wsk = new adres("Częstochowa", "Dąbrowskiego", 73);
-	cout <<  wsk << '\n';
-	cout << *wsk << '\n';
-	
-	adres a1(*wsk);
-	
-	if(wsk){
-		delete wsk;
-		wsk=nullptr;
+	string &miasto() { return adr->miasto1(); }
+
+	ostream &view(ostream &out) const
+	{
+		return out << im << ", " << wi << ", " << *adr << '\n';
 	}
-	
-	const adres* wsk1 = new adres("Łódź", "Piotrkowska", 33);
+
+	friend ostream &operator<<(ostream &out, const osoba &r);
+};
+ostream &operator<<(ostream &out, const osoba &r)
+{
+	return r.view(out);
+}
+int main()
+{
+#ifdef _WIN32
+	SetConsoleOutputCP(CP_UTF8);
+#endif
+	adres *wsk = new adres("Częstochowa", "Dąbrowskiego", 73);
+	cout << wsk << '\n';
+	cout << *wsk << '\n';
+
+	adres a1(*wsk);
+
+	if (wsk)
+	{
+		delete wsk;
+		wsk = nullptr;
+	}
+
+	const adres *wsk1 = new adres("Łódź", "Piotrkowska", 33);
 
 	cout << a1 << '\n';
 	cout << *wsk1 << '\n';
-	
+
 	adres a2;
 	cout << a2 << '\n';
 	a2 = a1;
 	cout << a2 << '\n';
-	
+
 	osoba os1("Ala", 25, *wsk1);
-	if(wsk1){
+	if (wsk1)
+	{
 		delete wsk1;
-		wsk1=nullptr;
+		wsk1 = nullptr;
 	}
 	cout << os1 << '\n';
 	osoba os2(os1);
 	cout << os2 << '\n';
-	
+
+	os1.miasto() = "Zmieniono miasto osoby 1.";
+	cout << os1 << '\n';
+
 	osoba os3;
 	cout << os3 << '\n';
 	os3 = os2;
-	cout << os3 << '\n';   
-	
-	#ifdef _WIN32
-    system("PAUSE");
-	#endif
+	cout << os3 << '\n';
+
+	os1.miasto() = "Drugi raz zmieniono miasto osoby 1.";
+	cout << os1 << '\n';
+
+#ifdef _WIN32
+	system("PAUSE");
+#endif
 	return 0;
 }
---
+++ Metody_programowania/zad3.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <cmath>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+using namespace std;
+
+class punkt
+{
+    double x_, y_;
+
+public:
+    punkt() : x_(0), y_(0) {}
+    punkt(double a1, double a2) : x_(a1), y_(a2) {}
+
+    double &x() { return x_; }
+    double &y() { return y_; }
+
+    double odleglosc(punkt a1)
+    {
+        double x = x_ - a1.x_;
+        double y = y_ - a1.y_;
+        double wynik = x * x + y * y;
+        return sqrt(wynik);
+    }
+
+    ostream &view(ostream &out) const
+    {
+        return out << x_ << " , " << y_ << endl;
+    }
+
+    friend ostream &operator<<(ostream &out, const punkt &r);
+};
+
+ostream &operator<<(ostream &out, const punkt &r)
+{
+    return r.view(out);
+}
+
+class wielobok
+{
+    size_t roz;
+    punkt *wsk;
+
+public:
+    wielobok() : roz(0), wsk(0) {}
+    wielobok(const punkt *b, const punkt *e) : roz(e - b > 0 ? e - b : 0), wsk(roz ? new punkt[roz] : 0)
+    {
+        for (int i = 0; i < roz; i++)
+        {
+            wsk[i] = b[i];
+        }
+    }
+
+    ~wielobok()
+    {
+        if (wsk)
+        {
+            delete[] wsk;
+            wsk = nullptr;
+        }
+    }
+    punkt &operator[](size_t i)
+    {
+        return wsk[i];
+    }
+    wielobok &operator=(const wielobok&r){
+        if(this!=&r){
+            roz=r.roz;
+            for(int i=0;i<roz;i++){
+            wsk[i]=r.wsk[i];
+            }
+        }
+        return *this;
+    }
+
+    double obwod()
+    {
+        double wynik = 0;
+        for (int i = 0; i < roz - 1; i++)
+        {
+            wynik += wsk[i].odleglosc(wsk[i + 1]);
+        }
+        wynik += wsk[roz - 1].odleglosc(wsk[0]);
+        return wynik;
+    }
+};
+
+int main()
+{
+
+    punkt p(2, 3);
+    cout << p.x() << ' ' << p.y() << '\n';
+    p.x() = 1;
+    p.y() = 1;
+    cout << p.x() << ' ' << p.y() << '\n';
+
+    cout << p.odleglosc(punkt()) << "\n\n";
+
+    const punkt t[] = {punkt(0, 0), punkt(0, 1), punkt(1, 1), punkt(1, 0)};
+    for (size_t i = 0; i < sizeof(t) / sizeof(punkt); ++i)
+        cout << i + 1 << ")  " << t[i] << endl;
+
+    cout << "\n*****\n";
+
+    wielobok w1(t, t + 4);
+    cout << w1.obwod() << '\n';
+
+    w1[1] = punkt(0.5, 0.5);
+    cout << w1.obwod() << '\n';
+    cout << "***\n\n";
+
+    wielobok w2;
+    w2 = wielobok(t, t + 3);
+    cout << w2.obwod() << '\n';
+    return 0;
+}
